4494856f372996d0fc365b202612b7878d3bb382
GeorgiBerkov <georgi.berkov@news.co.uk>
2025-02-21 20:07:47 +0200

    added report

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,8 @@ export default defineConfig({
   //workers: process.env.CI ? 1 : undefined,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html', ako ne raboti drugiq report tozi e defolten
+  reporter: [['junit', { outputFile: 'test-results/results.xml' }], ['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true, // Enable headless mode
