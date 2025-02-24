135bf7b409838a41f7987726f964e75c30c5c8c8
GeorgiBerkov <georgi.berkov@news.co.uk>
2025-02-24 15:28:58 +0200

    4 containers

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   //workers: 1, ---> keep only one worker when run tests locally
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html', ako ne raboti drugiq report tozi e defolten
