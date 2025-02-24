57346e61e700cfa3f750b613049a117750e2c351
GeorgiBerkov <georgi.berkov@news.co.uk>
2025-02-24 15:00:49 +0200

    added Parallel execution

--- playwright.config.js
+++ playwright.config.js
@@ -21,8 +21,8 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 1 : undefined,
-  workers: 1,
+  workers: process.env.CI ? 1 : undefined,
+  //workers: 1, ---> keep only one worker when run tests locally
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html', ako ne raboti drugiq report tozi e defolten
   reporter: [['junit', { outputFile: 'test-results/results.xml' }], ['html', { open: 'never' }]],
